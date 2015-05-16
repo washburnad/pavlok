8ff8e535b4566d24b6c30ac5f6d26fec2fef4e16
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-05-16 11:58:27 -0400

    Initial repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pavlok::Application.config.secret_key_base = 'f090bfef44a829ea0e6dafccd7c990725980d4c36bf7a468d91d4759021be8461b558de226e0259090a3f433a5de4298526c92cda09147cb61ae3adc779280fe'
