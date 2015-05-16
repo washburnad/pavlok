88fd43f712aab88c7c666cfc6d2646fc6e080752
Aaron Washburn <aaronwashburn@Aarons-MacBook-Pro.local>
2015-05-16 15:35:05 -0400

    Style page

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -3,12 +3,17 @@ $(document).ready( function() {
   var $min_walk_input = $("input[name='min_walk_time']");
   var $current_sit_span = $("#current_sit_time");
 
+  function updateSitTime(time) {
+    $current_sit_span.html(time);
+  }
 
   $max_sit_input.change( function() {
     console.log( 'max sit time changed ');
+    var max_sit_time = $max_sit_input.val();
   });
 
   $min_walk_input.change( function() {
     console.log( 'min walk time change ');
+    var min_walk_time = $min_walk_input.val();
   })
 })
